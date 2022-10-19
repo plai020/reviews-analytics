cfaa89581091305cf679ac83834aa123ea11cddf
PR Lai <plai0114@gmail.com>
2022-10-20 01:31:06 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -7,7 +7,14 @@ with open('reviews.txt', 'r') as f:
 		count += 1 #每讀取一筆，count就加1
 		if count % 1000 == 0: #每一千筆print一次
 			print(len(data))
-print(len(data))
-print(data[0])
-print('------------------')
-print(data[1])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+#計算留言平均長度
+#字串可以求長度，把每一個字串的長度加總存到sum_len
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+
+#計算平均長度
+average = sum_len / len(data)
+print('留言的平均長度為', average)
